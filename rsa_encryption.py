0ed797d822e6b093ec799ad076adf1b4477e1ba2
the-can-of-soup <campbell+github@campbellkeller.net>
2025-04-10 19:06:55 -0400

    Use sympy.divisors() in factorize()

--- rsa_encryption.py
+++ rsa_encryption.py
@@ -1,9 +1,8 @@
 from __future__ import annotations
 
-from typing import Callable
+from typing import Callable, Generator
 import secrets
 import sympy
-import math
 
 def random_prime(bits: int = 1024) -> int:
     """
@@ -27,6 +26,9 @@ def random_prime(bits: int = 1024) -> int:
 
 class PublicKey:
     def __init__(self, modulus: int, public_exponent: int = 65537):
+        assert modulus > 0
+        assert public_exponent > 0
+
         self.modulus: int = modulus
         self.exponent: int = public_exponent
 
@@ -38,6 +40,8 @@ class PublicKey:
 
 class PrivateKey:
     def __init__(self, p: int, q: int, public_exponent: int = 65537):
+        assert p != q
+
         self.p: int = p
         self.q: int = q
 
@@ -71,48 +75,32 @@ class PrivateKey:
             public_exponent=public_exponent
         )
 
-    @staticmethod
-    def factorize(public_key: PublicKey, progress_update: Callable[[int, int], None] | None = None, update_cooldown: int = 1_000_000) -> PrivateKey:
-        """
-        Performs a factorization attack to retrieve the private key from a given public key.
-        This isn't even remotely feasible for modern 2048-bit keys and should only be used on incredibly weak keys.
+def factorize(public_key: PublicKey, progress_update: Callable[[int, int], None] | None = None, update_cooldown: int = 1_000_000) -> PrivateKey:
+    """
+    Performs a factorization to retrieve the private key from a given public key.
+    This isn't feasible for modern 2048-bit keys and should only be used on very weak keys.
 
-        :param public_key: The public key to brute force.
-        :type public_key: PublicKey
-        :param progress_update: An optional function to be called periodically while cracking. Should be used for progress bars or displays. The function is passed the current number being checked and the maximum number that will be checked.
-        :type progress_update: Callable[[int, int], None]
-        :param update_cooldown: The number loops to process between each call to ``progress_update``.
-        :type update_cooldown: int
-        """
-        max_p: int = math.ceil(math.sqrt(public_key.modulus))
-        assert max_p >= 2
+    :param public_key: The public key to brute force.
+    :type public_key: PublicKey
+    :param progress_update: An optional function to be called periodically while cracking. Should be used for progress bars or displays. The function is passed the current number being checked and the maximum number that will be checked.
+    :type progress_update: Callable[[int, int], None]
+    :param update_cooldown: The number loops to process between each call to ``progress_update``.
+    :type update_cooldown: int
+    """
+    n: int = public_key.modulus
+    factors: Generator[int, None, None] = sympy.divisors(n, True)
 
-        if progress_update: # Use brute force code that includes update checks only if that feature is being used
-            current_update_cooldown: int = update_cooldown - 2
-            update_cooldown_expired: bool = False
+    p: int = 1
+    while p == 1 or p == n:
+        try:
+            p = next(factors)
+        except StopIteration:
+            break
+    else: # Runs when while loop exits normally
+        q = n // p
+        return PrivateKey(p, q, public_exponent=public_key.exponent)
 
-            for p in range(2, max_p + 1):
-                if update_cooldown_expired:
-                    progress_update(p, max_p)
-
-                # Check if p is a factor
-                if public_key.modulus % p == 0:
-                    return PrivateKey(p, public_key.modulus // p, public_key.exponent)
-
-                current_update_cooldown -= 1
-                update_cooldown_expired = current_update_cooldown <= 0
-                if update_cooldown_expired:
-                    current_update_cooldown = update_cooldown
-
-            progress_update(max_p, max_p)
-
-        else: # Use brute force code that does not include update checks if that feature isn't being used
-            for p in range(2, max_p + 1):
-                # Check if p is a factor
-                if public_key.modulus % p == 0:
-                    return PrivateKey(p, public_key.modulus // p, public_key.exponent)
-
-        raise ValueError('Something went horribly wrong and not a single valid private key was found.')
+    raise ValueError('Something went horribly wrong and not a single valid private key was found.')
 
 def encrypt_int(plaintext: int, public_key: PublicKey):
     """
@@ -127,16 +115,4 @@ def decrypt_int(ciphertext: int, private_key: PrivateKey):
     return pow(ciphertext, private_key.exponent, private_key.public_key.modulus)
 
 if __name__ == '__main__':
-    ...
-    # private_key: PrivateKey = PrivateKey.random()
-    # print(f'Random private key: {private_key}')
-    # print(f'Associated public key: {private_key.public_key}')
-    # print()
-    # n: int = 123456
-    # print(f'Original: {n}')
-    # n = encrypt_int(n, private_key.public_key)
-    # print(f'Encrypted: {n}')
-    # n = decrypt_int(n, private_key)
-    # print(f'Decrypted: {n}')
-    # input()
-    print(PrivateKey.factorize(PublicKey(1_000_000_000_100_000_000_002_379), progress_update=lambda p, max_p: print(f'{p}/{max_p}', end='\r')))
+    print(factorize(PublicKey(1_000_000_000_100_000_000_002_379), progress_update=lambda p, max_p: print(f'{p}/{max_p}', end='\r')))
