1d25d22f331a613da886746f8c0d101cdf344816
the-can-of-soup <campbell+github@campbellkeller.net>
2025-04-11 19:46:03 -0400

    Add encryption/decryption into bytestrings

--- rsa_encryption.py
+++ rsa_encryption.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Generator
+from typing import Generator, Any
 import secrets
 import sympy
 import math
@@ -30,13 +30,14 @@ class PublicKey:
         assert public_exponent > 0
 
         self.modulus: int = modulus
+        self.bit_length: int = modulus.bit_length()
         self.exponent: int = public_exponent
 
     def __repr__(self) -> str:
         return f'PublicKey({self.modulus}, exponent={self.exponent})'
 
     def __str__(self) -> str:
-        return f'{self.modulus} (exponent: {self.exponent})'
+        return f'{hex(self.modulus)} (exponent: {self.exponent})'
 
 class PrivateKey:
     def __init__(self, p: int, q: int, public_exponent: int = 65537):
@@ -60,7 +61,7 @@ class PrivateKey:
         return f'PrivateKey({self.p}, {self.q}, exponent={self.exponent})'
 
     def __str__(self) -> str:
-        return f'{self.p}, {self.q} (exponent: {self.exponent})'
+        return f'{hex(self.p)}, {hex(self.q)} (exponent: {self.exponent})'
 
     @staticmethod
     def random(bits: int = 2048, public_exponent: int = 65537) -> PrivateKey:
@@ -78,6 +79,18 @@ class PrivateKey:
 
         return PrivateKey(p, q, public_exponent=public_exponent)
 
+class CipherText:
+    def __init__(self, data: int, bit_length: int):
+        self.data: int = data
+        self.bit_length: int = bit_length
+        self.hex_length: int = math.ceil(self.bit_length / 4)
+
+    def __repr__(self) -> str:
+        return f'CipherText({self.data}, bit_length={self.bit_length})'
+
+    def __str__(self) -> str:
+        return f'0x{hex(self.data)[2:].zfill(self.hex_length)} (length: {self.bit_length} bits)'
+
 def factorize(public_key: PublicKey) -> PrivateKey:
     """
     Performs a factorization to retrieve the private key from a given public key.
@@ -85,9 +98,11 @@ def factorize(public_key: PublicKey) -> PrivateKey:
 
     :param public_key: The public key to brute force.
     :type public_key: PublicKey
+    :return: The associated private key.
+    :rtype: PrivateKey
     """
     n: int = public_key.modulus
-    factors: Generator[int, None, None] = sympy.divisors(n, True)
+    factors: Generator[int, Any, None] = sympy.divisors(n, True)
 
     p: int = 1
     while p == 1 or p == n:
@@ -101,7 +116,7 @@ def factorize(public_key: PublicKey) -> PrivateKey:
 
     raise ValueError('Something went horribly wrong and not a single valid private key was found.')
 
-def encrypt_int(plaintext: int, key: PublicKey | PrivateKey):
+def encrypt_int(plaintext: int, key: PublicKey | PrivateKey) -> CipherText:
     """
     Encrypts an integer.
     """
@@ -109,16 +124,176 @@ def encrypt_int(plaintext: int, key: PublicKey | PrivateKey):
     if isinstance(key, PrivateKey):
         public_key = key.public_key
 
-    return pow(plaintext, public_key.exponent, public_key.modulus)
+    assert plaintext >= 0
+    assert plaintext < public_key.modulus
 
-def decrypt_int(ciphertext: int, private_key: PrivateKey):
+    return CipherText(pow(plaintext, public_key.exponent, public_key.modulus), public_key.bit_length)
+
+def decrypt_int(ciphertext: CipherText, private_key: PrivateKey) -> int:
     """
     Decrypts an integer.
     """
     if isinstance(private_key, PublicKey):
         raise ValueError('Decryption requires a private key, not a public key!')
 
-    return pow(ciphertext, private_key.exponent, private_key.public_key.modulus)
+    return pow(ciphertext.data, private_key.exponent, private_key.public_key.modulus)
+
+def split_bytes_into_blocks(data: bytes, block_size: int) -> Generator[tuple[int, int], None, None]:
+    """
+    Splits a bytestring into binary blocks of a specified bit length.
+
+    :param data: The bytestring to split.
+    :type data: bytes
+    :param block_size: The maximum size of each block in bits.
+    :type block_size: int
+    :return: A generator that yields the block and its size in bits for each block.
+    :rtype: Generator[tuple[int, int], None, None]
+    """
+    assert block_size > 1
+
+    # Initialize
+    current_block: int = 1 # Add leading 1 to disambiguate block size
+    current_block_size: int = 1
+
+    # Loop through each byte
+    for byte in data:
+        # Insert byte
+        current_block <<= 8
+        current_block |= byte
+        current_block_size += 8
+
+        # Move to next block if block finished
+        while current_block_size >= block_size:
+            # Trim excess
+            excess_size: int = current_block_size - block_size
+            yield current_block >> excess_size, block_size
+
+            # Remove all bits from block except excess, which will be saved for the start of the next block
+            current_block &= (1 << excess_size) - 1
+            current_block |= (1 << excess_size) # Add leading 1 to disambiguate block size
+            current_block_size = excess_size + 1
+
+    if current_block_size > 0:
+        yield current_block, current_block_size
+
+def split_ciphertext_into_blocks(ciphertext: CipherText, block_size: int) -> Generator[int, None, None]:
+    """
+    Splits a ciphertext into binary blocks of a specified bit length.
+
+    :param ciphertext: The ciphertext to split.
+    :type ciphertext: CipherText
+    :param block_size: The maximum size of each block in bits.
+    :type block_size: int
+    :return: A generator that yields each block.
+    :rtype: Generator[int, None, None]
+    """
+    assert block_size > 0
+
+    # Initialize
+    data: int = ciphertext.data
+    block_count: int = math.ceil(ciphertext.bit_length / block_size)
+    block_mask: int = (1 << block_size) - 1
+
+    # Split
+    for i in range(block_count):
+        j: int = block_count - i - 1
+        yield (data >> (j * block_size)) & block_mask
+
+def encrypt_bytes(plaintext: bytes, key: PublicKey | PrivateKey, force_block_size: int | None = None) -> CipherText:
+    """
+    Encrypts a bytestring.
+
+    :param plaintext: The bytestring to encrypt.
+    :type plaintext: bytes
+    :param key: The public or private key to encrypt the data with.
+    :type key: PublicKey | PrivateKey
+    :param force_block_size: If not None, forces the encrypter to use this block size in bits.
+    :type force_block_size: int | None
+    :return: The encrypted bytestring.
+    :rtype: CipherText
+    """
+    # Get public key
+    public_key: PublicKey = key
+    if isinstance(key, PrivateKey):
+        public_key = key.public_key
+
+    # Calculate block size (in bits)
+    block_size: int = public_key.bit_length - 1
+    if force_block_size is not None:
+        assert force_block_size <= block_size
+        block_size = force_block_size
+
+    # Encrypt each block
+    ciphertext_data: int = 0
+    ciphertext_length: int = 0
+    blocks: Generator[tuple[int, int], None, None] = split_bytes_into_blocks(plaintext, block_size)
+    for block, _ in blocks:
+        # Encrypt
+        encrypted_block: CipherText = encrypt_int(block, public_key)
+
+        # Insert
+        ciphertext_data <<= encrypted_block.bit_length
+        ciphertext_data |= encrypted_block.data
+        ciphertext_length += encrypted_block.bit_length
+
+    return CipherText(ciphertext_data, ciphertext_length)
+
+def decrypt_bytes(ciphertext: CipherText, private_key: PrivateKey) -> bytes:
+    """
+    Decrypts a bytestring.
+
+    :param ciphertext: The encrypted data as an int.
+    :type ciphertext: CipherText
+    :param private_key: The private key to decrypt the data with.
+    :type private_key: PrivateKey
+    :return: The decrypted bytestring.
+    :rtype: bytes
+    """
+    if isinstance(private_key, PublicKey):
+        raise ValueError('Decryption requires a private key, not a public key!')
+
+    # Initialize
+    public_key: PublicKey = private_key.public_key
+    block_size: int = public_key.bit_length # Encrypted block size is always the same even if the decrypted block size is different
+
+    # Decrypt each block
+    plaintext: bytes = b''
+    byte_buffer: int = 0
+    byte_buffer_size: int = 0
+    blocks: Generator[int, None, None] = split_ciphertext_into_blocks(ciphertext, block_size)
+    for block in blocks:
+        # Decrypt
+        decrypted_block: int = decrypt_int(CipherText(block, block_size), private_key)
+
+        # Get block size
+        decrypted_block_size: int = decrypted_block.bit_length() - 1
+
+        # Remove the leading 1 that is placed to disambiguate block size
+        decrypted_block &= (1 << decrypted_block_size) - 1
+
+        # Add to buffer
+        byte_buffer <<= decrypted_block_size
+        byte_buffer |= decrypted_block
+        byte_buffer_size += decrypted_block_size
+
+        # Empty buffer
+        while byte_buffer_size >= 8:
+            byte: int = byte_buffer >> (byte_buffer_size - 8) # Grab byte
+            plaintext += bytes([byte])
+            byte_buffer &= (1 << (byte_buffer_size - 8)) - 1 # Remove byte from buffer
+            byte_buffer_size -= 8
+
+    return plaintext
 
 if __name__ == '__main__':
-    print(factorize(PublicKey(1_000_000_000_100_000_000_002_379)))
+    key: PrivateKey = PrivateKey.random(128)
+    print(f'Private key: {key}')
+    print()
+    text: bytes = b'Hello, World! This is a test of RSA block-based binary encryption.'
+    print(f'Original: {text}')
+    print()
+    text: CipherText = encrypt_bytes(text, key)
+    print(f'Encrypted: {text}')
+    print()
+    text: bytes = decrypt_bytes(text, key)
+    print(f'Decrypted: {text}')
